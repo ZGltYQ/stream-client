b6bb8699b6064f2c224d521388590ae72b096007
Dima <zgltyq@gmail.com>
2022-10-14 14:39:37 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import { useRef, useState } from 'react'
 import './App.css';
+import { Peer } from "peerjs";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+// import { loadPlayer } from './Player/index'
+import { JSMpeg } from './Player'
 
 function App() {
+  const canvas = useRef()
+  function handleStart() {
+      const peer = new Peer("client", {
+        host: '212.111.203.181',
+        port: 9000,
+        path: '/myapp'
+      });
+
+
+      const player = new JSMpeg.Player('pipe', {
+        canvas: canvas.current
+      });
+
+      peer.on("open", () => {
+        const conn = peer.connect("server");
+
+        conn.on("open", () => {
+          conn.on("data", (data) => {
+            player.write(data)
+          });
+        });
+      })
+      
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" onClick={handleStart}>start</Button>
+      <Container maxWidth="sm"><canvas ref={canvas}></canvas></Container>
     </div>
   );
 }
